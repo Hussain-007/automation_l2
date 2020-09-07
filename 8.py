adad47435eef7ba1e544b01d90267bc8dbc1fa4b
Aabid <mahe1879@gmail.com>
2020-09-07 16:35:14 +0530

    coded python 8.py and 9.py

--- 8.py
+++ 8.py
@@ -1,6 +1,16 @@
 __doc__ = 'Repeat program 7 with Tuple( Take example from Tutorial )'
 
-
+aTuple = ('apt', '-get', 'install ', 'python', '*', 1, 2, 3, 4, 5)
+bTuple = ('Kaptan', 'Aabid', 'Amrita','Aishwary', 'Gautam')
 
 if __name__ == '__main__':
-    print(find_prime(99))
+    #print length of Tuple
+    print(len(aTuple))
+    #print all elements of Tuple
+    print(*aTuple)
+    #print sliced element of Tuple
+    print(*aTuple[:4])
+    #perform repetition with * operator
+    print(aTuple[4]*10)
+    #Perform concatenation with other Tuple
+    print(aTuple + bTuple)
--- 9.py
+++ 9.py
@@ -1,6 +1,24 @@
 __doc__ = 'Write program to Add , subtract, multiply, divide 2 complex numbers.'
 
 
+def add(n1, n2):
+    print(f"{n1} + {n2} : {n1+n2}")
+
+def Subtract(n1, n2):
+    print(f"{n1} - {n2} : {n1-n2}")
+
+def Multiply(n1, n2):
+    print(f"{n1} * {n2} : {n1*n2}")
+
+def divide(n1, n2):
+    print("{} / {} : {:.2f}".format(n1, n2, n1/n2))
 
 if __name__ == '__main__':
-    print(find_prime(99))
+    #first complex number
+    c1 = 3 + 6j
+    #Second complex number
+    c2 = 6 + 15j
+    add(c1,c2)
+    Subtract(c1, c2)
+    Multiply(c1,c2)
+ 
